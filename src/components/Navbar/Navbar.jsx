36adb182b26ce8d5bb9d1145d6ccb1287fa31790
Lázaro Campos <lazaromanuel65@gmail.com>
2024-10-02 14:36:55 -0400

    [DELETE] logo-navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import "./Navbar.css";
-import logoImg from "../../images/logo.png";
 import {HiOutlineMenuAlt3} from "react-icons/hi";
 
 const Navbar = () => {
@@ -13,7 +12,6 @@ const Navbar = () => {
       <div className='container navbar-content flex'>
         <div className='brand-and-toggler flex flex-sb'>
           <Link to = "/" className='navbar-brand flex'>
-            <img src = {logoImg} alt = "site logo" />
             <span className='text-uppercase fw-7 fs-24 ls-1'>bookhub</span>
           </Link>
           <button type = "button" className='navbar-toggler-btn' onClick={handleNavbar}>
